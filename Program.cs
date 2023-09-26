64ea7ab7bf9a73de3c61d8049e7ae7b63a618ab7
projectfinalaudio <irfanghat@gmail.com>
2023-09-26 22:41:55 +0300

    LeetCode-125: Naive solution -> Source files

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode_125;
+
+class Program
+{
+    static void Main()
+    {
+        IsPalindrome("a.");
+    }
+
+    static bool IsPalindrome(string s)
+    {
+        // Specify splitPoints to use as split points
+        char[] splitPoints =
+        {
+            ' ', '(', ')', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '-', '+', '=', '|', '\\', '{', '}', '[',
+            ']', ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/', '_'
+        };
+        string[] words = s.Split(splitPoints);
+
+        // Merge array contents
+        string filtered = String.Join("", words);
+
+        // Convert filtered string to char array
+        char[] filteredCharacters = filtered.ToCharArray();
+
+        // Reverse the array
+        Array.Reverse(filteredCharacters);
+
+        // Store result as string
+        string reversed = new(filteredCharacters);
+
+        // Check if filtered and reversed string are equal
+        if (filtered.ToLower() == reversed.ToLower())
+        {
+            Console.WriteLine("The string is a palindrome...");
+            return true;
+        }
+
+        Console.WriteLine("{0}, {1}", filtered.ToLower(), reversed.ToLower());
+        Console.WriteLine("The string is not a palindrome...");
+        return false;
+    }
+}
